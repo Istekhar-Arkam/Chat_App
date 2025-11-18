4091029801a9c484fd76516b6256cba905e8b45c
istekhar <istekhararkam25@gmail.com>
2025-11-18 10:44:45 +0530

    fix: remove unused export of app from server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -61,8 +61,6 @@ if (process.env.NODE_ENV !== "production") {
   server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
 
-export { app };
-
 //export server for versal
 
 export default server;
