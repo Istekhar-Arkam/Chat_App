6b401891ec46eb543dd06e36c0f26c79e108a5f3
istekhar <istekhararkam25@gmail.com>
2025-11-18 13:34:08 +0530

    fix: update default PORT in server.js and remove hardcoded value in .env.sample

--- backend/src/server.js
+++ backend/src/server.js
@@ -53,7 +53,7 @@ app.use("/api/messages", messagesRouter);
 await connectDB();
 
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   server.listen(PORT, () => console.log("Server is running on port :"+ PORT));
 }
 
